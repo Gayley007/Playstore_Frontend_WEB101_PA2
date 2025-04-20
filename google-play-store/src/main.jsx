46a56b024e930acc0af2139f882420bbe7baa871
Gayley007 <gayleychoden@gmail.com>
2025-04-20 20:47:22 +1200

    changes before merging

--- google-play-store/src/main.jsx
+++ google-play-store/src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
-)
+  </React.StrictMode>
+);
